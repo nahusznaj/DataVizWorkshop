6a54d93397d925f8d05d7018ae1ed05bc1f5733d
DragonflyStats <kobriendublin@gmail.com>
2018-02-26 23:52:58 +0000

    Create contextualzoon.R

---
+++ ggExtra/contextualzoon.R
@@ -0,0 +1,12 @@
+# ggForce
+# Contextual zoom
+
+# Zooming in ggplot2 has always been done in one of two ways: By limiting the positional scale or by 
+# limiting the coordinate system. In the former actual data values are removed leading to a potential 
+# change in derived calculations (e.g. a fitted line had different parameters) while the later behaves
+# more as you would expect. ggforce provides a third option in the form of a new facetting 
+# function: facet_zoom(). Instead of describing it lets see how it works:
+
+ggplot(iris, aes(Petal.Length, Petal.Width, colour = Species)) +
+    geom_point() +
+    facet_zoom(x = Species == "versicolor")
