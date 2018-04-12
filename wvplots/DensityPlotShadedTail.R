7b6f7f9a4b7eaa89e14d5d214dfaf99271b5f183
DragonflyStats <kobriendublin@gmail.com>
2018-04-12 13:28:04 +0100

    Update DensityPlotShadedTail.R

--- wvplots/DensityPlotShadedTail.R
+++ wvplots/DensityPlotShadedTail.R
@@ -1,6 +1,17 @@
 
 set.seed(52523)
-d = data.frame(meas=rnorm(100))
+
+d = data.frame(meas=rnorm(1000))
+f = data.frame(meas=rnorm(1000))
+
 threshold = -1.5
-WVPlots::ShadedDensity(d, "meas", threshold, 
+
+######################################################
+
+
+ShadedDensity(d, "meas", threshold, 
                        title="Example shaded density plot, left tail")
+
+
+#####################################################
+
