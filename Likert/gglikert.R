7a9190f4dd6c08a7dfc57113ec0b1a042e487e61
DragonflyStats <kobriendublin@gmail.com>
2018-04-12 13:40:05 +0100

    Update gglikert.R

--- Likert/gglikert.R
+++ Likert/gglikert.R
@@ -4,10 +4,14 @@
 # A collection of miscellaneous tools and functions, such as tools to 
 # generate descriptive statistics tables, format output, visualize relations among variables or check distributions.
 
-Install the latest version of this package by entering the following in R:
+##########################################################
+
+# Install the latest version of this package by entering the following in R:
 install.packages("devtools")
 library(devtools)
 install_github("JWiley/JWileymisc")
+
+##########################################################
 
 testdat <- data.frame(
   Var = 1:4,
@@ -16,18 +20,24 @@ testdat <- data.frame(
   High = c("Sad", "Angry", "Hopeless", "Anxious"),
   stringsAsFactors = FALSE)
 
+##########################################################
+
 gglikert("Mean", "Var", "Low", "High", data = testdat, xlim = c(1, 5),
   title = "Example Plot of Average Affect Ratings")
 
+
+##########################################################
 testdat <- rbind(
   cbind(testdat, Group = "Young"),
   cbind(testdat, Group = "Old"))
 testdat$Mean[5:8] <- c(1.7, 2.6, 2.0, 4.4)
 
+##########################################################
 gglikert("Mean", "Var", "Low", "High", colour = "Group",
   data = testdat, xlim = c(1, 5),
   title = "Example Plot of Average Affect Ratings")
 
+##########################################################
 gglikert("Mean", "Var", "Low", "High", colour = "Group",
   data = testdat, xlim = c(1, 5),
   title = "Example Plot of Average Affect Ratings") +
