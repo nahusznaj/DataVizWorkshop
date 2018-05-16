04b01e9a267f40d8abbb12957441f3ff1eaa7caf
DragonflyStats <kobriendublin@gmail.com>
2018-05-16 15:29:32 +0100

    Create Setup.r

---
+++ Setup.r
@@ -0,0 +1,10 @@
+1. You need to reinstall a fresh version R. Version3.4.4
+Do not installt any packages until instructed
+Do nit inatall ggplot2 ir tidyvers
+You can downliad version 3.4.4 here
+Inatall ggplit from github.
+
+Library devtools
+Install_github(tidyverse/ggplir2)
+
+Ls(oackage ggplit)
